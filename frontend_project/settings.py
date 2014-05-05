40dade15516f14829faa55c9890a73b0891c787f
Michael Robbins <michael@dalmura.com.au>
2014-05-05 22:47:54 +1000

    Round 2

--- frontend_project/settings.py
+++ frontend_project/settings.py
@@ -27,10 +27,8 @@ except FileNotFoundError as e:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
